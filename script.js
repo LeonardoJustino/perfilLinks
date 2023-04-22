4d881e12511d832c171dac6295e74bfd38d42419
Leonardo Justino <leonardofjsantos024@gmail.com>
2023-04-22 16:34:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+  const html = document.documentElement /*documentElement é o html*/
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  
+  if (html.classList.contains("light")) {
+   
+    img.setAttribute("src", "/assets/fotoDePerfil.png")
+    img.setAttribute(
+      "alt",
+      "Logo representativa"
+    )
+  } else {
+    
+    img.setAttribute("src", "./assets/fotoDePerfil.png")
+  }
+}
