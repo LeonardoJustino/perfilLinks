7b6d2c15e81423bf8174623ba4b151bf0182ba7c
Leonardo Justino <leonardofjsantos024@gmail.com>
2023-04-22 16:52:32 -0300

    resolvendo

--- script.js
+++ script.js
@@ -3,19 +3,4 @@ function toggleMode() {
   html.classList.toggle("light")
 
   const img = document.querySelector("#profile img")
-  
-  if (html.classList.contains("light")) {
-   
-    img.setAttribute("src", "/assets/fotoDePerfil.png")
-    img.setAttribute(
-      "alt",
-      "Logo representativa"
-    )
-    }else{
-      img.setAttribute("src", "/assets/fotoDePerfil.png")
-    img.setAttribute(
-      "alt",
-      "Logo representativa"
-    )
-    }
 }
