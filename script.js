e774d8070fd50c64a3291e719289d2450a39afbe
Leonardo Justino <leonardofjsantos024@gmail.com>
2023-04-22 16:55:12 -0300

    remove  button

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleMode() {
-  const html = document.documentElement /*documentElement é o html*/
-  html.classList.toggle("light")
-
-  const img = document.querySelector("#profile img")
-}
